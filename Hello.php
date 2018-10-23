c416c0a83a84345030a86796d145ae8be7ab9bab
NamekoMadoka <nkmetmk888@gmail.com>
2018-10-23 20:38:50 +0900

    Initial commit

---
+++ Hello.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>てーきゅう</title>
+        <link rel="stylesheet" href="Hello.css">
+        <link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
+    </head>
+    <body>
+        <header class="header-bg">
+            <nav>
+
+
+                <header class="header">
+                    <div class="header-logo"><img src="logo2.png" alt="てーきゅう"></div>
+                    <div class="header-list">
+                        <ul>
+                            <li>TOP</li>
+                            <li>NEWS</li>
+                            <li>STORY</li>
+                            <li>ONAIR</li>
+                        </ul>
+                    </div>
+                </header>
+            </nav>
+        </header>
+        <h1 class="T-2">Many Many Money Money!!!</h1>
+        <h1 class="T-4"><?php echo "Fat!? to Shangri-la"; ?></h1>
+
+    </body>
+</html>
